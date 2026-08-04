98f7130d14395c001f3307714843be06a458dc4c
SrSubbulakshmi <84225242+SrSubbulakshmi@users.noreply.github.com>
2026-08-04 12:51:54 -0600

    Create LocatorNames.java

---
+++ src/main/java/couriermobileapp/enums/LocatorNames.java
@@ -0,0 +1,15 @@
+package couriermobileapp.enums;
+
+public enum LocatorNames {
+    accessibility("accessibility"),
+    iOSClassChain("iOSClassChain"),
+    className("className"),
+    xpath("xpath"),
+    uiAutomator("uiAutomator"),
+    iOSNsPredicate("iOSNsPredicate");
+
+    String locatorName;
+    LocatorNames(String locatorName) {
+        this.locatorName = locatorName;
+    }
+}
