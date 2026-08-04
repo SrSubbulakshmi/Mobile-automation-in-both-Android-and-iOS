24e9f2f07f5d31504f7bcf1312f5b7543e723554
SrSubbulakshmi <84225242+SrSubbulakshmi@users.noreply.github.com>
2026-08-04 12:57:23 -0600

    Create MobileCourierKeys.java

---
+++ src/test/java/couriermobileaprp/tests/BaseTest/MobileCourierKeys.java
@@ -0,0 +1,6 @@
+package couriermobileapp.tests.BaseTest;
+
+public interface MobileCourierKeys {
+    String COURIER_EMAIL = "orderFlow.delco.courier.email";
+    String COURIER_PASSWORD = "orderFlow.delco.courier.password";
+}
